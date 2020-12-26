523ca05149e159709cab2994f8cc8bd2e2d86f6f
minhyeok <mahns1201@gmail.com>
2020-12-26 21:02:04 +0900

    [test]test2

--- server.py
+++ server.py
@@ -1,11 +1,44 @@
-from flask import Flask
+# server.py
+from flask import Flask, request, jsonify
+import sys
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/keyboard')
+def Keyboard():
+    dataSend = {
+    "Subject":"OSSP",
+    "user":"corona_chatbot"
+    }
+    return jsonify(dataSend)
 
-def hello():
-    return "hello, world!"
+@app.route('/message', methods=['POST'])
+
+def Message():
+    content = request.get_json()
+    content = content['userRequest']
+    content = content['utterance']
+
+    if content == u"안녕":
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type" : "basicCard",
+                            "items": [
+                                {
+                                    "title" : "",
+                                    "description" : "안녕하세요"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    return jsonify(dataSend)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0') # Flask 기본포트 5000번
