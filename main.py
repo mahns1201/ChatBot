e59a07f202406bfd5cf96dc2c99022b8a3bc878e
minhyeok <mahns1201@gmail.com>
2020-12-28 13:37:18 +0900

    [test] test4

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 from flask import Flask, request, jsonify
 import sys
-
 app = Flask(__name__)
+
 
 @app.route('/keyboard')
 def Keyboard():
     dataSend = {
-    "Subject":"OSSP",
-    "user":"corona_chatbot"
     }
     return jsonify(dataSend)
 
 @app.route('/message', methods=['POST'])
-
 def Message():
+    
     content = request.get_json()
     content = content['userRequest']
     content = content['utterance']
-
+    
     if content == u"안녕":
         dataSend = {
             "version": "2.0",
@@ -29,9 +27,22 @@ def Message():
                             "items": [
                                 {
                                     "title" : "",
-                                    "description" : "안녕하세요"
+                                    "description" : "안녕"
                                 }
                             ]
+                        }
+                    }
+                ]
+            }
+        }
+    else :
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText":{
+                            "text" : "아직 공부하고있습니다."
                         }
                     }
                 ]
@@ -40,4 +51,4 @@ def Message():
     return jsonify(dataSend)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0') # Flask 기본포트 5000번
+    app.run(host='0.0.0.0')
--- server.py
+++ server.py
@@ -93,6 +93,6 @@ def true_or_false():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port = 5000)
 
     # host="0.0.0.0", port=5000
