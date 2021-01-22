3ea7fe3c3b28ee6fc07b977aa8a2818c9919818a
mahns <mahns1201@gmail.com>
2021-01-22 15:45:41 +0900

    [temp] test

--- test.py
+++ test.py
@@ -14,16 +14,29 @@ def coffee():
     coffee_menu = req["action"]["detailParams"]["coffee_menu"]["value"]    # read json file
                 # req["action"]["detailParams"][파리미터 명]["value"]
 
-    answer = coffee_menu
-    # answer = "아메리카노"
+    url = https://github.com/meotitda/DICTIONARY/blob/master/DIC/A/AST.md
+    # answer = 
 
     res = {
         "version": "2.0",
         "template": {
             "outputs": [
                 {
-                    "simpleText": {
-                        "text": answer
+                    "basicCard": {
+                        "title": "title",
+                        "description": "description",
+                        "thumbnail": {
+                            "imageUrl": url
+                        },
+                    },
+                    "profile": {
+                        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4BJ9LU4Ikr_EvZLmijfcjzQKMRCJ2bO3A8SVKNuQ78zu2KOqM",
+                        "nickname": "보물상자"
+                    },
+                    "social": {
+                        "like": 1238,
+                        "comment": 8,
+                        "share": 780
                     }
                 }
             ]
