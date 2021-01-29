9d185344f046e03986997cea3f214841a22c58dc
mahns <mahns1201@gmail.com>
2021-01-29 22:39:26 +0900

    [temp]

--- test.py
+++ test.py
@@ -25,35 +25,16 @@ def movies():
         "version": "2.0",
         "template": {
             "outputs": [
-            {
                 "basicCard": {
-                "title": "보물상자",
-                "description": "보물상자 안에는 뭐가 있을까",
-                "thumbnail": {
-                    "imageUrl": url
-                },
-                "social": {
-                    "like": 1238,
-                    "comment": 8,
-                    "share": 780
-                },
-                "buttons": [
-                    {
-                    "action": "message",
-                    "label": "열어보기",
-                    "messageText": "짜잔! 우리가 찾던 보물입니다"
+                    "title": "보물상자",
+                    "description": "보물상자 안에는 뭐가 있을까",
+                    "thumbnail": {
+                        "imageUrl": url
                     },
-                    {
-                    "action":  "webLink",
-                    "label": "구경하기",
-                    "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
-                    }
-                ]
                 }
-            }
             ]
         }
-    }
+    }            
 
     return jsonify(res)
 
